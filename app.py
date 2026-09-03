b063e41caec9cf075c12ab1d84607990ef40f3ef
samuel aza <samuelazapoveda2004@gmail.com>
2026-09-03 01:45:30 -0500

    [vista][semana-3] agrega plantilla html y formulario de creacion de personaje en python

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect
 from models.personaje import Personaje
 
 app = Flask(__name__)
@@ -8,24 +8,18 @@ lista_personajes = []
 
 @app.route('/personajes', methods=['GET'])
 def obtener_personajes():
-    personajes_dict = [p.to_dict() for p in lista_personajes]
-    return jsonify(personajes_dict)
+    return render_template('personajes.html', lista_personajes=lista_personajes)
 
 
 @app.route('/personajes', methods=['POST'])
 def crear_personaje():
-    datos = request.get_json()
-    
-    nombre = datos.get('nombre')
-    clase = datos.get('clase')
-    
+    nombre = request.form.get('nombre')
+    clase = request.form.get('clase')
+
     nuevo_heroe = Personaje(nombre, clase)
     lista_personajes.append(nuevo_heroe)
-    
-    return jsonify({
-        "mensaje": "Personaje creado con éxito",
-        "personaje": nuevo_heroe.to_dict()
-    }), 201
+
+    return redirect('/personajes')
 
 
 if __name__ == '__main__':
